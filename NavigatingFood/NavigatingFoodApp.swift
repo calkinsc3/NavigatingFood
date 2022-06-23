384ea0dc1105f8d43ff9dcb9f0285ae2e7f0f0b0
Bill Calkins <calkinsc3@gmail.com>
2022-06-23 12:19:08 -0500

    Initial Commit

---
+++ NavigatingFood/NavigatingFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigatingFoodApp.swift
+//  NavigatingFood
+//
+//  Created by Bill Calkins on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigatingFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
